0813f4f587efab1abf23fbf519799e5e1493b8f2
Andriy Polukhin <andriy.polukhin@gmail.com>
2021-05-06 10:12:07 +0300

    clean up and basic dependencies added

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/**
+Client Side render app: react (cra)
+   -> database which is Firebase
+   -> react-loading-skeleton
+   -> tailwind
+
+	Folder structure
+ 	 src:
+		-> components,
+		-> constants,
+		-> context,
+		-> helpers,
+		-> lib (firebase is going to live in here),
+		-> services (firebase functions in here)
+*/
