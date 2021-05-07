2f7e645ed9c3889420b4a13a48dbbcb46cc87926
Andriy Polukhin <andriy.polukhin@gmail.com>
2021-05-07 21:35:39 +0300

    added signup, notfound and services functions

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -4,30 +4,60 @@ import FirebaseContext from '../context/firebase';
 
 import * as ROUTES from '../constants/routes';
 
-const Login = () => {
+import { doesUsernameExists } from '../services/firebase';
+
+const Signup = () => {
 	const history = useHistory();
 	const { firebase } = useContext(FirebaseContext);
+	const [username, setUsername] = useState('');
+	const [fullName, setFullName] = useState('');
 	const [emailAddress, setEmailAddress] = useState('');
 	const [password, setPassword] = useState('');
 
 	const [error, setError] = useState('');
 	const isInvalid = password === '' || emailAddress === '';
 
-	const handleLogin = async (e) => {
+	const handleSignUp = async (e) => {
 		e.preventDefault();
-		try {
-			await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-			history.push(ROUTES.DASHBOARD);
-		} catch (error) {
-			setEmailAddress('');
-			setPassword('');
-			setError(error.message);
+
+		const usernameExists = await doesUsernameExists(username);
+		if (!usernameExists) {
+			try {
+				const createdUserResult = await firebase
+					.auth()
+					.createUserWithEmailAndPassword(emailAddress, password);
+
+				// authentication
+				// => emailAddress & password * username (displayName)
+				await createdUserResult.user.updateProfile({
+					displayName: username
+				});
+				// * firebase user collection ( create a document )
+				await firebase.firestore().collection('users').add({
+					userId: createdUserResult.user.uid,
+					username: username.toLowerCase(),
+					fullName,
+					emailAddress: emailAddress.toLowerCase(),
+					following: [],
+					dateCreated: Date.now()
+				});
+				history.push(ROUTES.DASHBOARD);
+			} catch (error) {
+				setFullName('');
+				setEmailAddress('');
+				setPassword('');
+				setError(error.message);
+			}
+		} else {
+			setUsername('');
+			setError('That username is already taken, please try another.');
 		}
 	};
 
 	useEffect(() => {
-		document.title = 'Login - Instagram';
+		document.title = 'Sign Up - Instagram';
 	}, []);
+
 	return (
 		<div className='container flex mx-auto max-w-screen-md items-center h-screen'>
 			<div className='flex w-3/5'>
@@ -48,10 +78,26 @@ const Login = () => {
 					</h1>
 					{error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
 
-					<form onSubmit={handleLogin} method='POST'>
+					<form onSubmit={handleSignUp} method='POST'>
+						<input
+							aria-label='Enter your username'
+							type='text'
+							placeholder='Username'
+							className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+							onChange={({ target }) => setUsername(target.value)}
+							value={username}
+						/>
+						<input
+							aria-label='Enter your full name'
+							type='text'
+							placeholder='Full name'
+							className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+							onChange={({ target }) => setFullName(target.value)}
+							value={fullName}
+						/>
 						<input
 							aria-label='Enter your email address'
-							type='text'
+							type='email'
 							placeholder='Email address'
 							className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
 							onChange={({ target }) => setEmailAddress(target.value)}
@@ -73,15 +119,15 @@ const Login = () => {
 								isInvalid && 'opacity-50'
 							}`}
 						>
-							Log in
+							Sign Up
 						</button>
 					</form>
 				</div>
 				<div className='flex flex-col justify-content items-center  w-full bg-white p-4 border border-gray-primary'>
 					<p className='text-sm'>
-						Don't have an account?{` `}
-						<Link to={ROUTES.SIGN_UP} className='font-bold text-blue-medium'>
-							Sign Up
+						Already have an account?{` `}
+						<Link to={ROUTES.LOGIN} className='font-bold text-blue-medium'>
+							Login
 						</Link>
 					</p>
 				</div>
@@ -90,10 +136,4 @@ const Login = () => {
 	);
 };
 
-export default Login;
-
-// * Todo: add to tailwind.config
-// bg-blue-medium -> hex values
-//  text-red-primary -> hex values
-//  text-gray-base -> hex values
-//  text-gray-primary -> hex values
+export default Signup;
